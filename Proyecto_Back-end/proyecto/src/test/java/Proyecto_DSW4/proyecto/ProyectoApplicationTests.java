6690d62d0b444302e7188644b31e572ddc655bf0
Adriel Alejandro Marín Silva <adrielms21@gmail.com>
2026-09-25 13:09:14 -0600

    Add files via upload

---
+++ Proyecto_Back-end/proyecto/src/test/java/Proyecto_DSW4/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto_DSW4.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
